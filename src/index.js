3905efb24a57f23bcbe08343c40cd19bdff8704a
Roger Nordqvist <progdev.squirrel@gmail.com>
2017-07-05 23:42:32 +0200

    refactored the app component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCDGBsTO0UkEzYlYjivYDU_6Z1AOQRtguM';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      videos: []
+    };
+
+    /* initial search */
+    YTSearch({key: API_KEY, term: 'path of exile'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
