f335d9763a93f03afb6cdca4dd447ef6d1b475d0
Roger Nordqvist <progdev.squirrel@gmail.com>
2017-07-06 22:34:43 +0200

    added css styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,10 +9,10 @@ class SearchBar extends Component {
 
   render() {
     return(
-      <div>
+      <div className="search-bar">
         {/* set the state */}
         <input
-          value={this.state.term} 
+          value={this.state.term}
           onChange={event => this.setState({ term: event.target.value })} />
       </div>
     );
