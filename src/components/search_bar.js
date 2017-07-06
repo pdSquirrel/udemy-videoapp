56b6a48f8f4072ff303116e91c4d4da1b4a44163
Roger Nordqvist <progdev.squirrel@gmail.com>
2017-07-06 22:45:38 +0200

    added search functionality

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,9 +13,14 @@ class SearchBar extends Component {
         {/* set the state */}
         <input
           value={this.state.term}
-          onChange={event => this.setState({ term: event.target.value })} />
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
+  }
+
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
   }
 }
 
